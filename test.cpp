29f1a2b4b7b9a594bc9e4d49a5badf55216df148
Robin Qu <robinqu@gmail.com>
2024-06-23 05:20:50 +0000

    chores: test with thread counts

--- test.cpp
+++ test.cpp
@@ -17,58 +17,37 @@ int main() {
           "## 4. Create your Endpoint by clicking **Create Endpoint**. By default, your Endpoint is created with a medium CPU (2 x 4GB vCPUs with Intel Xeon Ice Lake) The cost estimate assumes the Endpoint will be up for an entire month, and does not take autoscaling into account.\n\n<img src=\"https://raw.githubusercontent.com/huggingface/hf-endpoints-documentation/main/assets/1_create_cost.png\" alt=\"create endpoint\" />\n\n## 5. Wait for the Endpoint to build, initialize and run which can take between 1 to 5 minutes.\n\n<img src=\"https://raw.githubusercontent.com/huggingface/hf-endpoints-documentation/main/assets/overview.png\" alt=\"overview\" />\n\n## 6. Test your Endpoint in the overview with the Inference widget \ud83c\udfc1 \ud83c\udf89!)";
 
     {
-        ModelLoader model_loader {"/Users/robinqu/Workspace/modelscope/judd2024/chatllm_quantized_models/bge-reranker-m3-q4_1.bin"};
-
+        ModelLoader model_loader {"/root/CLionProjects/chatllm_quantized_models/bge-reranker-m3-q4_1.bin"};
         ModelFactory::Result result;
         ModelFactory::load(model_loader, result, {});
         std::vector<int> ids;
         result.tokenizer->encode_qa("hello", text, ids);
-
-        {
+        for(const auto n: {1,10,(int)std::thread::hardware_concurrency()}){
             GenerationConfig config;
-            config.num_threads = std::thread::hardware_concurrency();
+            config.num_threads = n;
             const auto t1 = std::chrono::system_clock::now();
             result.model->qa_rank(config, ids);
             std::cout << "qa_rank: num_threads=" << config.num_threads <<  ", elapsed=" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-t1).count() << std::endl;
         }
-
-        {
-            GenerationConfig config;
-            config.num_threads = 1;
-            const auto t1 = std::chrono::system_clock::now();
-            result.model->qa_rank(config, ids);
-            std::cout << "qa_rank: num_threads=1, elapsed=" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-t1).count() << std::endl;
-        }
     }
 
     {
-        ModelLoader model_loader {"/Users/robinqu/Workspace/modelscope/judd2024/chatllm_quantized_models/bge-m3-q4_1.bin"};
-
+        ModelLoader model_loader {"/root/CLionProjects/chatllm_quantized_models/bge-reranker-m3-q4_1.bin"};
         ModelFactory::Result result;
         ModelFactory::load(model_loader, result, {});
-
         std::vector<int> ids;
         result.tokenizer->encode_qa("hello", text, ids);
-
-        {
+        for(const auto n: {1,10,(int)std::thread::hardware_concurrency()}){
             GenerationConfig config;
-            config.num_threads = std::thread::hardware_concurrency();
+            config.num_threads = n;
             std::vector<float> embedding;
             const auto t1 = std::chrono::system_clock::now();
             result.model->text_embedding(config, ids, embedding);
             std::cout << "text_embedding: num_threads=" << config.num_threads <<  ", elapsed=" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-t1).count() << std::endl;
         }
-
-        {
-            GenerationConfig config;
-            config.num_threads = 1;
-            std::vector<float> embedding;
-            const auto t1 = std::chrono::system_clock::now();
-            result.model->text_embedding(config, ids, embedding);
-            std::cout << "text_embedding: num_threads=" << config.num_threads <<  ", elapsed=" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-t1).count() << std::endl;
-        }
-
     }
+
+
 
 
 }
